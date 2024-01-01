b3cd99f602a375d9c62b2e554f3e13a3d6372db2
Amol Shinde <shindeamol409@gmail.com>
2024-01-01 16:29:23 +0530

    Added new file.

---
+++ Chapter02/index.js
@@ -0,0 +1,12 @@
+//var myName = "Amol Shinde";
+//console.log(myName);
+
+/* var myAge = 26;
+console.log(myAge); */
+
+/* var amIAmol = true;
+console.log(amIAmol); */
+
+//typeof operator
+//console.log(typeof (myName));
+/* console.log(typeof (myAge)); */
---
+++ PracticeSets/PracticeSet2.js
@@ -0,0 +1,10 @@
+//console.log(10 + "20"); //Output - 1020
+//console.log(9 - "5"); //Output - 4 bug doesn't concatenate
+//console.log("Java" + "Script"); //Output - JavaScript
+//console.log(" " + " "); //Output - Empty string
+//console.log(" " + 0); //Output - 0
+//console.log("Amol" - "Shinde"); //Output - NaN
+//console.log(true + true); //Output - 2
+//console.log(true + false); //Output - 1
+//console.log(false + true); //Output - 1
+console.log(false - true); // Output : -1
