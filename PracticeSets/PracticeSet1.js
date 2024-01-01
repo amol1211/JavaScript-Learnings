893efa8acae24844d49255aa576db8afc3c93bb6
Amol Shinde <shindeamol409@gmail.com>
2024-01-01 15:23:02 +0530

    Added new file.

---
+++ PracticeSets/PracticeSet1.js
@@ -0,0 +1,9 @@
+// Variable naming practice
+
+/* var _myName = "Amol"; */ // Valid
+/* var 1myName = "Shinde"; */ // Invalid
+/* var _1my__Name = "AmolTheGreat"; */ //Valid
+/* var $myName = "Amol Shinde"; */ //Valid
+/* var myName% = "Amol Shinde"; */ //Invalid
+
+console.log($myName);
