96a6112f92124eb2488feb3ce923f7b1401539aa
Amol Shinde <shindeamol409@gmail.com>
2024-01-08 01:44:44 +0530

    Added new file.

---
+++ Chapter03/index.js
@@ -0,0 +1,43 @@
+//Arithmatic operator
+
+/* console.log(3 + 3);
+console.log(10 - 5);
+console.log(20 / 5);
+console.log(5 * 6);
+
+console.log("Remainder Operator " + (27 % 4)); */
+
+/* ---------------------------------------------------- */
+
+//Increment and Decrement Operator
+//Operator : x++ or ++x or x-- or --x
+//If used postfix, with operator after operand (for example, x++),
+//the increment operator increments and returns the value before incrementing.
+
+/* var num = 15;
+var newNum = num++;
+console.log(num); // 16
+console.log(newNum); //15 */
+
+/* If use prefix , with operator before operand (for example, ++x),
+the increment operator increments and returns the value after incrementing.
+
+Prefix increment operator means the variable is incremented first and 
+then the expression is evaluated using the new value of the variable.*/
+
+/* var num = 15;
+var newNum = ++num;
+console.log(num); // 16
+console.log(newNum); // 16 */
+
+//Postfix decrement
+
+/* var num = 15;
+var newNum = num--;
+console.log(num); //14
+console.log(newNum); //15 */
+
+var num = 15;
+var newNum = --num;
+console.log(num); //15
+console.log(newNum); //14
