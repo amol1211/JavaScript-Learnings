2ee805e9945a2f544335c8d60b139c074a6740b4
Amol Shinde <shindeamol409@gmail.com>
2024-01-08 00:28:28 +0530

    Added new file.

---
+++ PracticeSets/PracticeSet3.js
@@ -0,0 +1,9 @@
+//NaN Practice
+
+/* NaN === NaN; */ //False
+/* Number.NaN === NaN; */ // False
+/* isNaN(NaN); */ //True
+/* isNaN(Number.NaN); */ //True
+/* Number.isNaN(NaN); */ //True
+
+console.log(Number.isNaN(NaN));
