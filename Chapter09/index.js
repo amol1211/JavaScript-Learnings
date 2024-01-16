e1ac2acb13437a82fb99ee4b2479b8730aac6ac1
Amol Shinde <shindeamol409@gmail.com>
2024-01-16 21:22:44 +0530

    Added new file.

---
+++ Chapter09/index.js
@@ -0,0 +1,58 @@
+//Math.PI
+/* console.log(Math.PI); */ //Output : 3.141592653589793
+
+//Math.round
+//Returns the value of x rounded to its nearest integer
+/* let num = 10.2565;
+console.log(Math.round(num)); */ //Output : 10
+
+//Math.pow()
+//pow(x, y) returns the value of x to the power of y
+
+/* console.log(Math.pow(2, 3)); //Output : 8
+console.log(2 ** 3); */ //Output : 8
+
+//Math.sqrt()
+//Math.sqrt(x) returns the square root of x
+
+/* console.log(Math.sqrt(25));
+console.log(Math.sqrt(81));
+console.log(Math.sqrt(66)); */
+
+//Math.abs()
+//Math.abs() returns the absolute (positive) value of x
+
+/* console.log(Math.abs(-55));
+console.log(Math.abs(-55.5));
+console.log(Math.abs(-955));
+console.log(Math.abs(4 - 6)); */
+
+//Math.ceil()
+//Math.ceil(x) returns the value of x rounded up to its nearest integer
+
+/* console.log(Math.ceil(4.51)); //Output : 5
+console.log(Math.round(4.51)); //Output : 5
+console.log(Math.ceil(99.1)); //Output : 100
+console.log(Math.round(99.1)); //Output : 99 */
+
+//Math.floor()
+//Math.floor(x) returns the value of x rounded down to its nearest integer
+
+/* console.log(Math.floor(4.7)); // Output : 4
+console.log(Math.floor(99.9)); // Output : 99 */
+
+//Math.max()
+//Can be used to find the highest value in a list of arguments
+
+/* console.log(Math.max(0, 150, 30, 20, -8, -200)); */
+
+//Math.random()
+//It returns a random number between 0 to 1
+
+/* console.log(Math.random() * 10); */
+
+//Math.trunc()
+//The trunc() method returns the integer part of a number
+
+/* console.log(Math.trunc(4.6)); //Output : 4
+console.log(Math.trunc(-99.1)); //Output : -99 */
---
+++ PracticeSets/PracticeSet11.js
@@ -0,0 +1,7 @@
+//Practice Time
+
+//If the argument is a positive number, Math.truc() is equivalent to Math.floor(),
+//otherwise, Math.trunc() is equivalent to Math.ceil().
+
+console.log(Math.trunc(4.6)); //Output : 4
+console.log(Math.trunc(-99.1)); //Output : -99
