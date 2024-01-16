c229506f3dc63fc2748dbf1a4f29439944177eef
Amol Shinde <shindeamol409@gmail.com>
2024-01-16 15:54:35 +0530

    Added new file.

--- Chapter08/index.js
+++ Chapter08/index.js
@@ -1,9 +1,16 @@
 //new Date()
 //Date objects are created with the new Date() constructor.
 
-let currDate = new Date();
+/* let currDate = new Date();
 console.log(currDate);
 
 console.log(new Date());
 console.log(new Date().toLocaleString()); //1/13/2024, 10:13:28 PM
 console.log(new Date().toString()); //Sat Jan 13 2024 22:13:28 GMT+0530 (India Standard Time)
+console.log(Date.now()); //returns milliseconds
+ */
+
+/* var d = new Date(2021, 0, 5, 10, 33, 30, 0);
+console.log(d.toLocaleString()); */
+
+//How to set individual date
