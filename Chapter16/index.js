8e52bba5408d28bbffc83eb32f45435d32d72b66
Amol Shinde <shindeamol409@gmail.com>
2024-02-05 20:43:01 +0530

    Added new file.

--- Chapter16/index.js
+++ Chapter16/index.js
@@ -36,3 +36,23 @@ const sPerson = { ...person };
 
 console.log(person);
 console.log(sPerson); */
+
+/* const person = { name: "Fred", age: 87 };
+ */ // console.log(Object.values(person));
+/* const arrObj = Object.entries(person);
+console.log(Object.fromEntries(arrObj)); */
+
+// ES10/ECMAScript2019
+//Array.prototype.{Float32Array,flatMap}
+//Object.fromEntries()
+
+//ES11/ECMAScript2020
+// 1. BigInt
+
+let oldNum = Number.MAX_SAFE_INTEGER;
+/* console.log(oldNum);*/
+console.log(9007199254740991n + 12n);
+const newNum = 9007199254740991n + 12n;
+
+console.log(newNum);
+console.log(typeof newNum);
