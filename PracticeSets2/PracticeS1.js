d15aee0f532af1f4c1eba153293afb63b9209ba7
Amol Shinde <shindeamol409@gmail.com>
2024-05-20 10:29:25 +0530

    Added new files.

--- PracticeSets2/PracticeS1.js
+++ PracticeSets2/PracticeS1.js
@@ -34,7 +34,7 @@ console.log(username); */
 
 //Create the same function using Arrow function
 
-const countVowels = (str) => {
+/* const countVowels = (str) => {
   let count = 0;
   for (const char of str) {
     if (
@@ -50,3 +50,70 @@ const countVowels = (str) => {
 
   return count;
 };
+ */
+
+/*-----------------------------------------*/
+
+//We're given array of marks of students. Filter out the marks of students which are 90+
+
+/* let marks = [97, 64, 32, 49, 99, 96, 86];
+
+let toppersArray = marks.filter((val) => {
+  return val > 90;
+});
+
+console.log(toppersArray); */
+
+/*-----------------------------------------*/
+
+/*Take a number n as input from user. Create an array of numbers from 1 to n.
+Use the reduce method to calculate sum of all numbers in the array.
+Use the reduce method to calculate product of all numbers in the array.*/
+
+/* let n = prompt("Enter a number : ");
+
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+  arr[i - 1] = i;
+}
+
+console.log(arr);
+
+let sum = arr.reduce((res, curr) => {
+  return res + curr;
+});
+
+console.log("sum = ", sum);
+
+let factorial = arr.reduce((res, curr) => {
+  return res * curr;
+});
+
+console.log("factorial = ", factorial);
+ */
+
+/* Create a h1 heading element with text of your choice. Append more text to it using JS */
+
+/* let h1 = document.querySelector("h1");
+
+console.dir(h1.innerText);
+
+h1.innerText = h1.innerText + " is genius!"; */
+
+/* ---------------------------------- */
+
+/* Create 3 divs with common class name- "box". Access them and add some unique text to each of them */
+
+let divs = document.querySelectorAll(".box");
+/* console.log(divs[0]);*/
+
+let idx = 1;
+for (let div of divs) {
+  div.innerText = `new unique value ${idx}`;
+  idx++;
+}
+
+/* divs[0].innerText = "new unique val 1";
+divs[1].innerText = "new unique val 2";
+divs[2].innerText = "new unique val 3"; */
